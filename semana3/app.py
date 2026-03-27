ee8a0c81dab36085684379ffcbd420276087c257
Ricardo031 <rjtb31@gmail.com>
2026-03-26 21:26:36 -0500

    semana 3: Task 3

--- semana3/app.py
+++ semana3/app.py
@@ -2,9 +2,8 @@
 Guardar y cargar el inventario desde archivos CSV para conservar los datos entre sesiones, compartirlos y consultar estadísticas del negocio.
 Aplicar listas, diccionarios y tuplas junto con módulos y funciones en Python para construir un inventario modular y persistente: operaciones CRUD, estadísticas y lectura/escritura de archivos CSV con validaciones y manejo de errores.
 """
-import csv 
 import servicios
-
+import csv 
 # Inicialización del inventario como un diccionario vacío
 inventory = {}
 option = None
@@ -14,7 +13,76 @@ while option != 0:
     try:
         # Solicitar opción al usuario
         option = int(input("Add product (1), view inventory (2), search product (3), Update product (4), delete (5), calculate statistics (6) or exit (0):"))
+        if option == 0:
+            break
+        elif option == 1:
+            count = int(input("Enter the quantity of the product do you want to add: "))
+            if count <= 0:
+                print("Invalid quantity. Please enter a positive integer.")
+                continue
+            for i in range(count):
+                name = input(f"Enter product name {i+1}: ").strip()
+                try:
+                    price = float(input(f"Enter product price {i+1}: "))
+                except ValueError:
+                    print("Invalid price. Please enter a valid number.")
+                    continue
+                if price <= 0:
+                    print("Invalid price. Please enter a positive number.")
+                    continue
+                quantity = int(input(f"Enter product quantity {i+1}: "))
+                if quantity <= 0:
+                    print("Invalid quantity. Please enter a positive integer.")
+                    continue
+                else:
+                    servicios.add_product(inventory, name, price, quantity)
+
+        elif option == 2:
+                servicios.show_invetory(inventory)
+        
+        elif option == 3:
+            if not inventory:
+                print(f"there is not any product in the inventory.")
+                continue    
+            name = input("Enter product name: ").strip()
+            result = servicios.search_product(inventory, name)
+            print(result)
+        
+        elif option == 4:
+            name = input("Enter product name: ").strip()
+            if name in inventory: 
+                print(f"Product '{name}' found.")
+                try:
+                    new_price = float(input("Enter new price: "))
+                except ValueError:
+                    print("Invalid price. Please enter a valid number.")
+                    continue
+                if new_price <= 0:
+                    print("Invalid price. Please enter a positive number.")
+                    continue
+                try:
+                    new_quantity = int(input("Enter new quantity: "))
+                except ValueError:
+                    print("Invalid quantity. Please enter a valid number.")
+                    continue
+                if new_quantity <= 0:
+                    print("Invalid quantity. Please enter a positive integer.")
+                    continue
+            else:
+                print(f"Product '{name}' not found or does not exist.")
+                continue
+            servicios.update_product(inventory, name, new_price, new_quantity)
+        
+        elif option == 5:
+            name = input("Enter product name: ").strip()
+            servicios.delete_product(inventory, name)
+
+        elif option == 6:
+            statistics = servicios.calculate_statistics(inventory)
+            print(statistics)
+
     except ValueError:
         # Manejo de error para entradas no numéricas
         print("Invalid input. Please enter a valid integer.")
         continue
+
--- semana3/servicios.py
+++ semana3/servicios.py
@@ -1,10 +1,11 @@
 #utilizar la extension better comments para que puedan ver mejor los comentarios que coloque en el codigo
 #* 2. Estructura de datos y modularización
+#? agregar producto
 def add_product(inventory, name, price, quantity):
     """
     Agrega un nuevo producto al inventario.
     #?Args:
-        inventory (dict): El diccionario del inventario.
+        inventory (dict): Agregar el producto al inventario.
         name (str): El nombre del producto.
         price (float): El precio del producto.
         quantity (int): La cantidad disponible.
@@ -14,9 +15,16 @@ def add_product(inventory, name, price, quantity):
         print(f"The product '{name}' already exists. Use update_product to modify it.")
     else:
         # Agregar nuevo producto al diccionario
-        inventory[name] = {'price': price, 'quantity': quantity}
-        print(f"Product '{name}' added successfully.")
-        
+        product = {
+            "price": price,
+            "quantity": quantity,
+        } 
+        inventory[name] = product
+    print(f"Product '{name}' added successfully.")
+    print("-----------------------------------------------")
+
+
+#? mostrar inventario
 def show_invetory(inventory):
     """
     Muestra todos los productos en el inventario.
@@ -30,25 +38,44 @@ def show_invetory(inventory):
         # Recorrer y mostrar cada producto
         print("Current Intentory:")
         for name, details in inventory.items():
-            print(f"Name: {name} Price: {details['price']} Quantity: {details['quantity']}")
+            total = details['price'] * details['quantity']
+            print(f"Name: '{name}' Price: {details['price']:.2f} | Quantity: {details['quantity']} | Total: {total:.2f}")
+        print("-----------------------------------------------")
 
+#? buscar producto
 def search_product(inventory, name):
     """
     Busca un producto por su nombre y muestra su información.
+    Si no se encuentra, pregunta nuevamente hasta que el usuario decida salir.
 
     #?Args:
         inventory (dict): El diccionario del inventario.
-        name (str): El nombre del producto a buscar.
-    """
-    if name in inventory:
-        # Mostrar detalles del producto encontrado
-        print(f"Product {name} found. Price: {inventory[name]['price']:.2f}")
-        print(f"Quantity: {inventory[name]['quantity']}")
-    else:
-        # Informar si no se encontró
-        print(f"Product {name} not found.")
-        return None
+        name (str): El nombre del producto a buscar inicialmente.
+    """    
+    current_name = name
+    while True:
+        if current_name in inventory:
+            # Producto encontrado: mostrar detalles y salir de la función
+            print(f"\nProduct '{current_name}' found.")
+            print(f"Name: '{current_name}' | Price: {inventory[current_name]['price']:.2f} | Quantity: {inventory[current_name]['quantity']}")
+            return f"Success. if you want to continue searching, press '3'."
+        # Producto no encontrado o nombre vacío
+        if current_name == "":
+            print("\nInvalid input. Please enter a valid product name.")
+        else:
+            print(f"\nProduct '{current_name}' not found or does not exist.")
+        
+        # Preguntar si desea intentar con otro nombre o salir
+        leave = input("Do you want to try searching for another product? (Enter new name or 'exit' to cancel): ").strip()
+        
+        if leave.lower() == "exit":
+            return f"Search for '{current_name}' completed."
+          
+        
+        # Actualizar el nombre para la siguiente iteración
+        current_name = leave
 
+#? actualizar producto
 def update_product(inventory, name, new_price=None, new_quantity=None):
     """
     Actualiza el precio o la cantidad de un producto existente.
@@ -59,19 +86,21 @@ def update_product(inventory, name, new_price=None, new_quantity=None):
         new_quantity (int, optional): La nueva cantidad. Por defecto es None.
     """
     if name in inventory:
+        
         # Actualizar precio si se proporciona
         if new_price is not None:
             inventory[name]['price'] = new_price
-            print(f"Price of {name} updated to {new_price:.2f}")
+            print(f"Price of '{name}' updated to {new_price:.2f}")
         # Actualizar cantidad si se proporciona
         if new_quantity is not None:
             inventory[name]['quantity'] = new_quantity
-            print(f"Quantity of {name} updated to {new_quantity}")
+            print(f"Quantity of '{name}' updated to {new_quantity}")
     else:
         # Producto no encontrado
-        print(f"Product {name} not found.")
+        print(f"Product '{name}' not found or does not exist.")
 
-def delete_product(inventory, name):
+#? eliminar producto
+def delete_product(inventory, name): 
     """
     Elimina un producto del inventario.
     #?Args:
@@ -81,12 +110,13 @@ def delete_product(inventory, name):
     if name in inventory:
         # Eliminar entrada del diccionario
         del inventory[name]
-        print(f"Product {name} deleted successfully.")
+        print(f"Product '{name}' deleted successfully.")
     else:
         # Producto no encontrado
-        print(f"Product {name} not found.")
+        print(f"Product '{name}' not found or does not exist.")
 
-def calculate_total(inventory): #return tupla/dict 
+#? calcular estadísticas
+def calculate_statistics(inventory): #return tupla/dict 
     """
     Calcula el valor total del inventario y la cantidad total de productos.
     #?Args:
@@ -96,11 +126,24 @@ def calculate_total(inventory): #return tupla/dict
     """
     total_price = 0
     total_quantity = 0
+    product_most_exp_name = ""
+    product_most_exp_price = 0
+    product_most_exp_quantity = 0
+#* 3.Estadísticas del inventario
+    if not inventory:
+        # Caso inventario vacío
+        return "The inventory is empty."
     # Iterar sobre todos los productos para sumar valores
     for name, details in inventory.items():
+        if details['price'] > product_most_exp_price:
+            product_most_exp_price = details['price']
+            product_most_exp_name = name
+            product_most_exp_quantity = details['quantity']
         total_price += details['price'] * details['quantity']
         total_quantity += details['quantity']
-    # Retornar los totales acumulados
-    return (total_price, total_quantity)
+    print(f"total price: {total_price:.2f}")
+    print(f"total quantity: {total_quantity}")
+    print(f"product most expensive price is {product_most_exp_name}: {product_most_exp_price:.2f}")
+    print(f"product most expensive quantity is {product_most_exp_name}: {product_most_exp_quantity}")
 
 #todo: seguir con las demas funciones faltantes
